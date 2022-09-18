2f9ec9dec371c8400ed91ac5f62a1b360debcd2b
kushal118 <irfankushal02@gmail.com>
2022-09-18 03:27:05 +0200

    removing console.log and alert

--- js/app.js
+++ js/app.js
@@ -13,36 +13,35 @@
  * JS Standard: ESlint
  * 
 */
-alert('hello world')
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
-const mainNavUl =document.querySelector("#navbar__list");
+ */
+const mainNavUl = document.querySelector("#navbar__list");
 const navLinks = document.querySelectorAll('a');
-const allSection= document.querySelectorAll(".section")
-const inputs =document.querySelectorAll("input")
-const form =document.querySelector('form');
+const allSection = document.querySelectorAll(".section")
+const inputs = document.querySelectorAll("input")
+const form = document.querySelector('form');
 const name = document.querySelector('#name');
-const email =document.querySelector('#email');
-const phone =document.querySelector('#phone')
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone')
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
-function createLink(listText, anchorLink){
-    let  anchorTag= document.createElement('a');
-    anchorTag.href =anchorLink;
+ */
+function createLink(listText, anchorLink) {
+    let anchorTag = document.createElement('a');
+    anchorTag.href = anchorLink;
     anchorTag.innerHTML = listText;
     return anchorTag;
-  }
+}
 
 
 
@@ -50,13 +49,13 @@ function createLink(listText, anchorLink){
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 
 // build the nav
-allSection.forEach(section=>{
-    let listItem =document.createElement('li');
-    let linkListItem=createLink(section.dataset.nav,section.dataset.nav);
+allSection.forEach(section => {
+    let listItem = document.createElement('li');
+    let linkListItem = createLink(section.dataset.nav, section.dataset.nav);
     linkListItem.classList.add('menu__link');
     listItem.appendChild(linkListItem)
     mainNavUl.appendChild(listItem)
@@ -64,25 +63,25 @@ allSection.forEach(section=>{
 
 
 // Add class 'active' to section when near top of viewport
-function viewPortMeasure(el){
+function viewPortMeasure(el) {
     const viewSize = el.getBoundingClientRect();
-    return viewSize.top< window.innerHeight*0.45 && viewSize.bottom > window.innerHeight*0.45;
+    return viewSize.top < window.innerHeight * 0.45 && viewSize.bottom > window.innerHeight * 0.45;
 }
 
-document.addEventListener('scroll',function(){
-    const navBarList =document.querySelectorAll('#navbar__list a');
-    allSection.forEach(section =>{
-        if(viewPortMeasure(section)){
+document.addEventListener('scroll', function () {
+    const navBarList = document.querySelectorAll('#navbar__list a');
+    allSection.forEach(section => {
+        if (viewPortMeasure(section)) {
             console.log("viewport works");
             section.classList.add('your-active-class');
-            navBarList.forEach(nav=>{
-                if(nav.textContent==section.id){
+            navBarList.forEach(nav => {
+                if (nav.textContent == section.id) {
                     nav.classList.add('active')
-                }else{
+                } else {
                     nav.classList.remove('active')
                 }
             })
-        }else{
+        } else {
             section.classList.remove('your-active-class')
         }
     })
@@ -94,25 +93,27 @@ document.addEventListener('scroll',function(){
 const listTags = document.querySelectorAll('ul a');
 
 // Scroll smooth to section on link click
-listTags.forEach( listTag=> {
+listTags.forEach(listTag => {
     listTag.addEventListener('click', function (e) {
         //prevent default "jump" from 'a' element
         console.log(listTags);
         e.preventDefault();
-        console.log(e);
         allSection.forEach(section => {
-            if (section.id == listTag.getAttribute('href')){
-                section.scrollIntoView({behavior: "smooth", inline: "nearest"});
+            if (section.id == listTag.getAttribute('href')) {
+                section.scrollIntoView({
+                    behavior: "smooth",
+                    inline: "nearest"
+                });
             }
-        })  
+        })
     })
-    });
+});
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 
@@ -120,13 +121,12 @@ listTags.forEach( listTag=> {
 
 // Set sections as active
 // Put alert when someone submit form
-form.addEventListener('submit',(e)=>
-{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if(inputs.value !=""){
+    if (inputs.value != "") {
         alert(`Thank you ${name.value} filling the information, we have receive your email ${email.value}`)
-    }else{'You have to fill up your name and email'
+    } else {
+        'You have to fill up your name and email'
 
     }
-}
-)
+})
